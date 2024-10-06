c7b81e50fbd7487239c93f83b012614a3924433c
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-10-06 13:20:17 +0200

    primer_commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,18 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { MalagaDescriptionComponent } from '../components/malaga-description/malaga-description.component';
+import { MalagaImageComponent } from '../components/malaga-image/malaga-image.component';
+import { MalagaInformationComponent } from '../components/malaga-information/malaga-information.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    MalagaDescriptionComponent,
+    MalagaImageComponent,
+    MalagaInformationComponent
+  ]
 })
 export class HomePageModule {}
